4743f3261c810dbfbcdb822bd5465d3e3a32eba2
Chris Zeigler <c.l.zeigler@navitaire.com>
2017-04-03 15:41:31 -0600

    Added test project

---
+++ Life/LifeTest/LifeUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace LifeTest
+{
+    public class LifeUnitTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(4, 2+2);
+        }
+    }
+}
---
+++ Life/LifeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LifeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amadeus")]
+[assembly: AssemblyProduct("LifeTest")]
+[assembly: AssemblyCopyright("Copyright © Amadeus 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3965b99-e84c-4b4d-8a16-6a31b6af4174")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
