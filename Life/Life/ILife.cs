57862ae104f11a191c75cf67368e3cecede70c0b
Chris Zeigler <c.l.zeigler@navitaire.com>
2017-04-04 09:33:11 -0600

    Removed implementation and tests.

--- Life/Life/ILife.cs
+++ Life/Life/ILife.cs
@@ -12,21 +12,5 @@ namespace Life
     public interface ILife
     {
 
-        // ***********************************************************************************
-        //  class       -   class Life
-        //  puurpose    -   provides a container and some functionality for calculating states   
-        //                  during a session of Conway's Game of Life
-        //  API         -   int Height get      -   property
-        //                  int Width get       -   property
-        //                  int Ticks get       -   property
-        //                  bool this[int, int] -   indexer 
-        //                  Tick()              -   method  
-        // ***********************************************************************************
-
-        void Tick();
-        int Ticks { get; }
-        int Height { get; }
-        int Width { get; }
-        bool this[int y, int x] { get; set; }
     }
 }
